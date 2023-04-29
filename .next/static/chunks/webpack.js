ad686d98579751c4d7dc298187998be6146c91af
Paula <paulasalinor@gmail.com>
2023-04-28 22:47:24 -0300

    add storybook

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "680ebe9892f2bc88"; }
+/******/ 		__webpack_require__.h = function() { return "a7213f149d6307fc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
